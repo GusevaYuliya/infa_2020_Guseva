9aaaa35da959f1742b553ea836f58f4acd224841
Guseva Yuliya <guseva.yuliya2002@yandex.ru>
2020-11-07 10:52:15 +0300

    шарик на поверхности один, работает

--- lab5/ball.py
+++ lab5/ball.py
@@ -1,12 +1,15 @@
 import pygame
 from pygame.draw import *
 from random import randint
+
 pygame.init()
 
-FPS = 1
-screen = pygame.display.set_mode((1200, 900))
+FPS = 60
+screen = pygame.display.set_mode((1200, 800))
 
-'''задаем цвет'''
+maxx = 1200
+maxy = 800
+
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -16,38 +19,49 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
+def ballwindow(x,y,r):
+    window = pygame.Surface((2*r, 2*r))
+    window.fill((0, 0, 0))
+    circle(window, color, (r, r), r)
+    screen.blit(window, (x, y))
 
-def new_ball():
-    '''рисует новый шарик '''
-    global x, y, r
-    x = randint(100,700)
-    y = randint(100,500)
-    r = randint(30,50)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
-
-def ballxyr(event):
-    '''записывает координаты круга'''
-    print(x, y, r)
-
-pygame.display.update()
-clock = pygame.time.Clock()
-finished = False
+cdvig = [-3, -2, -1, 1, 2, 3]
 
 sum = 0
+
+
+clock = pygame.time.Clock()
+finished = False
+click = False
+
 while not finished:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print('Click!')
-            ballxyr(event)
-            if (x-event.pos[0])**2 + (y - event.pos[1])**2 <= r**2:
-                sum += 1
-                print('счет ', sum)
-    new_ball()
-    pygame.display.update()
-    screen.fill(BLACK)
+    x = randint(100, 700)
+    y = randint(100, 500)
+    r = randint(30, 50)
+    color = COLORS[randint(0, 5)]
+    vx = cdvig[randint(0, 5)]
+    vy = cdvig[randint(0, 5)]
+    click = False
+    while (finished == False) and (click == False):
+        ballwindow(x, y, r)
+        x = x + vx
+        y = y + vy
+        clock.tick(FPS)
+        if x + (2*r) >= maxx or x <= 0:
+            vx = vx * (-1)
+        if y + (2 * r) >= maxy or y <= 0:
+            vy = vy * (-1)
+        pygame.display.update()
+        screen.fill(BLACK)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print('Click!')
+                if ((x+r) - event.pos[0]) ** 2 + ((y+r) - event.pos[1]) ** 2 <= r ** 2:
+                    sum += 1
+                    print('счет ', sum)
+                    click = True
+
 
 pygame.quit()
